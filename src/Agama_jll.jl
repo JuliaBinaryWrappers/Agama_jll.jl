28fa07dda21d7bd761281d9d7c83389f2270c08c
jlbuild <juliabuildbot@gmail.com>
2025-07-28 23:52:18 +0000

    Agama_jll build 1.0.0+0

---
+++ src/Agama_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule Agama_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("Agama")
+JLLWrappers.@generate_main_file("Agama", UUID("966baf2c-d1f1-5710-8d7e-4d538f29c110"))
+end  # module Agama_jll
