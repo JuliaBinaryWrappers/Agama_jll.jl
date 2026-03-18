0c1c91209f8493a2e3dc10cdd24d5fa0bece4043
jlbuild <juliabuildbot@gmail.com>
2026-03-18 00:28:55 +0000

    Agama_jll build 1.1.0+0

--- src/Agama_jll.jl
+++ src/Agama_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Agama")
-JLLWrappers.@generate_main_file("Agama", UUID("966baf2c-d1f1-5710-8d7e-4d538f29c110"))
+JLLWrappers.@generate_main_file("Agama", Base.UUID("966baf2c-d1f1-5710-8d7e-4d538f29c110"))
 end  # module Agama_jll
